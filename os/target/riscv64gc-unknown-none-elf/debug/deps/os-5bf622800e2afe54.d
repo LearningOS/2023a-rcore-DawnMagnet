b446fdbd113ff0e8f9bfaf75a4cd0812df7fa3d1
DawnMagnet <5643840+dawn_magnet@user.noreply.gitee.com>
2023-10-23 14:12:34 +0800

    my change

--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.d
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.d
@@ -1,5 +1,6 @@
-/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.rmeta: src/main.rs
+/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.rmeta: src/main.rs src/sbi.rs
 
-/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.d: src/main.rs
+/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-5bf622800e2afe54.d: src/main.rs src/sbi.rs
 
 src/main.rs:
+src/sbi.rs:
--- os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.d
+++ os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.d
@@ -1,5 +1,6 @@
-/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.rmeta: src/main.rs
+/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.rmeta: src/main.rs src/sbi.rs
 
-/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.d: src/main.rs
+/home/sgct/github-classroom/LearningOS/2023a-rcore-DawnMagnet/os/target/riscv64gc-unknown-none-elf/debug/deps/os-9aeea44ea656f0cd.d: src/main.rs src/sbi.rs
 
 src/main.rs:
+src/sbi.rs:
